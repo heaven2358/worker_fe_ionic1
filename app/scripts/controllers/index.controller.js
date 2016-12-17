624cfe956efd77ff4d7540e6b5ba5c25ab117e7a
ck <153293261@qq.com>
2016-12-17 21:55:08 +0800

    申请工作页面

--- app/scripts/controllers/index.controller.js
+++ app/scripts/controllers/index.controller.js
@@ -1,18 +1,18 @@
 angular.module('worker').controller('indexCtrl',['$scope','$rootScope','$location','$state','apiService', function($scope, $rootScope, $location, $state, apiService) {
     $scope.busy = false;
-
+    $scope.ifGetLocation = false;
+    $scope.data = {};
     $scope.$on( '$ionicView.afterEnter', function(event, data){
-        window.tools.setNativeTitle( '人人催' );
         $scope.init();
-    } );
+    });
 
     $scope.$on( '$ionicView.leave', function(event, data) {
         $rootScope.bottomBtnType = 0;
-    } );
+    });
 
     $scope.$on( '$ionicView.loaded', function(event, data){
         //$scope.init();
-    } );
+    });
 
     $scope.init = function() {
         $rootScope.rootTap = false;
@@ -41,4 +41,15 @@ angular.module('worker').controller('indexCtrl',['$scope','$rootScope','$locatio
         $rootScope.rootRole = ret;
         $location.path('wantOffer');
     }
+
+    window.wx.getLocation({
+        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+            console.log(res);
+            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            var speed = res.speed; // 速度，以米/每秒计
+            var accuracy = res.accuracy; // 位置精度
+        }
+    });
 }]);
